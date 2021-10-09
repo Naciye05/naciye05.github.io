62130e6062422eb72c8c6e0fecf336781343000c
Ömer Faruk Erdoğan <naciye.erd.005@gmail.com>
2021-10-09 23:34:30 +0300

    Some changes

--- js/banner.js
+++ js/banner.js
@@ -1,6 +1,7 @@
 const banner = `
 <a href="/" id="bannerTitle">Gano ile Sağlık</a>
-<a class="logo" id="firstLogo" href="https://www.fb.com/nnacis/" title="Facebook"><img src="images/facebook.png"></a>
+<a href="https://docs.google.com/forms/d/e/1FAIpQLSfOnYYitAsae_cU2PGXx-XwtfGwWwJUGukTf7uw4QS0gncUMw/viewform" id="register">Kayıt Ol</a>
+<a class="logo" href="https://www.fb.com/nnacis/" title="Facebook"><img src="images/facebook.png"></a>
 <a class="logo" href="https://www.instagram.com/gano.excel.05/" title="Instagram"><img src="images/instagram.png"></a>
 `;
 
