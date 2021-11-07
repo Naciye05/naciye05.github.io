2f5bab7e4192178951a7f75906fac3a5135b6f13
Naciye05 <92056276+Naciye05@users.noreply.github.com>
2021-11-07 14:11:03 +0300

    Update banner.js

--- js/banner.js
+++ js/banner.js
@@ -1,6 +1,6 @@
 const banner = `
 <a href="/" id="bannerTitle">Gano ile Sağlık</a>
-<a href="https://docs.google.com/forms/d/e/1FAIpQLSfOnYYitAsae_cU2PGXx-XwtfGwWwJUGukTf7uw4QS0gncUMw/viewform" id="register">Kayıt Ol</a>
+<a href="https://bit.ly/3H1kxxO" id="register">Kayıt Ol</a>
 <a class="logo" href="https://www.fb.com/nnacis/" title="Facebook"><img src="images/facebook.png"></a>
 <a class="logo" href="https://www.instagram.com/gano.excel.05/" title="Instagram"><img src="images/instagram.png"></a>
 `;
